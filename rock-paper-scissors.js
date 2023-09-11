2633cd3ee8857b6d9420f9b37a33cf84a2abde3c
Tim-PF <tim.pfops@gmail.com>
2023-09-11 13:17:29 +0200

    Added a simple UI to display results

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,20 +1,32 @@
 const choice = ["Rock", "Paper", "Scissors"]
 
 
+let playerWins = 0;
+let computerWins = 0;
+
+let score =  `Player: ${playerWins} Computer:${computerWins}` ;
 
 
-game();
 
 
 
+// function game() {
+//  for (let i = 0; i < 1; i++) {
+//     console.log(playRound(getComputerChoice(), getPlayerChoice()));
+//  }
+// }
 
-function game(gameNumber = 5) {
- for (let i = 0; i < gameNumber; i++) {
-    console.log(playRound(getComputerChoice(), getPlayerChoice()));
- }
+function checkWinner() {
+    if (playerWins >= 5) {
+        return "Player Won"
+    }
+    else if (computerWins >= 5) {
+        return "Computer Wins"
+    }
+    else {
+        return false;
+    }
 }
-
-
 function getComputerChoice() {
     number = Math.floor((Math.random() * 3));
     return choice[number].toLowerCase();
@@ -35,9 +47,11 @@ function getPlayerChoice() {
 function playRound(computer , player) {
     if (computer === "rock") {
         if (player === "paper") {
+            playerWins += 1;
             return "You Won";
         }
         else if ( player === "scissors") {
+            computerWins += 1;
             return "You Lose";
         }
         else {
@@ -49,21 +63,25 @@ function playRound(computer , player) {
             return "Tie";
         }
         else if ( player === "scissors") {
+            playerWins += 1;
             return "You Won";
         }
         else {
-            return "You Lose"
+            computerWins += 1;
+            return "You Lose";
         }
     }
     //scissors
     else {
         if (player === "paper") {
+            computerWins += 1;
             return "You Lose";
         }
         else if ( player === "scissors") {
             return "Tie";
         }
         else {
+            playerWins += 1;
             return "You Won"
           
        }
@@ -71,3 +89,35 @@ function playRound(computer , player) {
     
  
 }
+
+const playerButton = document.querySelectorAll('button');
+
+
+const div = document.createElement('div');
+div.style.backgroundColor = 'pink';
+
+// Create a new p (paragraph) element
+const textResult = document.createElement('p');
+
+// Append the p element to the div
+div.appendChild(textResult);
+
+// Append the div to the document body
+document.body.appendChild(div);
+
+playerButton.forEach( (button) => {
+  button.addEventListener('click', function(e) {
+    const computer = getComputerChoice();
+    const player = e.target.innerText;
+    result = playRound(computer , player)
+    score =  `Player: ${playerWins} Computer:${computerWins}`
+    textResult.textContent = `${result} ${score}`;
+    if (!checkWinner() == false) {
+        winner = document.createElement('p');
+        winner.textContent = checkWinner();
+        div.appendChild(winner);
+    };
+  });
+});
+
+
